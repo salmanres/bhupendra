7eb284170828de6cf62e193410cd0ff4f39c466c
salman_siddeeque <salmanres@gmail.com>
2025-09-22 19:26:22 +0530

    change

--- index.js
+++ index.js
@@ -3,20 +3,16 @@ const app = express();
 const port = 4500;
 require('./database/connection');
 const cors = require('cors');
-const usersdata = require('./schema/UserSchema');
+const userdata = require('./schema/UserSchema');
 
-app.use(cors());
+app.use(cors()); //middleware pipeline req / res pass
 app.use(express.json());
-
-app.get('/', (req, res)=>{
-    res.send('this is my first server that i created');
-});
 
 app.post('/register', async (req, res)=>{
     try{
         console.log(req.body);
-        await usersdata.insertOne(req.body);
-        res.send('user registered successfully');
+        await userdata.insertOne(req.body);
+        res.send('registration successful');
     }catch(err){
         console.log(err);
     };
--- schema/UserSchema.js
+++ schema/UserSchema.js
@@ -1,19 +1,16 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    name:{
+    username: {
         type: String
     },
-    email:{
-        type:String
+    email: {
+        type: String
     },
-    mobile:{
-        type:Number
-    },
-    password:{
-        type:String
+    password: {
+        type: String
     }
 });
 
-const usersdata = new mongoose.model('user', userSchema);
-module.exports = usersdata;
+const userdata = new mongoose.model('newuser', userSchema);
+module.exports = userdata;
