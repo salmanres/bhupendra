6f8de2f6791e7cc1df48ab729efa2ce71c20c283
salman_siddeeque <salmanres@gmail.com>
2025-09-26 19:19:46 +0530

    change

--- index.js
+++ index.js
@@ -8,57 +8,76 @@ const userdata = require('./schema/UserSchema');
 app.use(cors()); //middleware pipeline req / res pass
 app.use(express.json());
 
-app.post('/register', async (req, res)=>{
-    try{
+app.post('/register', async (req, res) => {
+    try {
         console.log(req.body);
         await userdata.insertOne(req.body);
         res.send('registration successful');
-    }catch(err){
+    } catch (err) {
         console.log(err);
     };
 });
 
-app.get('/userdata', async (req, res)=>{
-    try{
-       const data = await userdata.find();
-       console.log(data);
-       res.send(data);
-    }catch(err){
+app.get('/userdata', async (req, res) => {
+    try {
+        const data = await userdata.find();
+        console.log(data);
+        res.send(data);
+    } catch (err) {
         console.log(err);
     };
 });
 
-app.delete('/deleteuser/:id', async (req, res)=>{
-    try{
-        const {id} = req.params;
+app.delete('/deleteuser/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
         console.log(id);
         await userdata.findByIdAndDelete(id);
         res.send('user info deleted!');
-    }catch(err){
+    } catch (err) {
         console.log(err);
     };
 });
 
-app.get('/userbyid/:id', async(req, res)=>{
-    try{
-        const {id} = req.params;
-        const data = await userdata.find({_id : id});
+app.get('/userbyid/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const data = await userdata.find({ _id: id });
         res.send(data);
-    }catch(err){
+    } catch (err) {
         console.log(err);
     };
 })
 
-app.put('/updatebyid/:id', async(req, res)=>{
-    try{
-        const {id} = req.params;
+app.put('/updatebyid/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
         await userdata.findByIdAndUpdate(id, req.body);
         res.send('data updated');
-    }catch(err){
+    } catch (err) {
         console.log(err);
     };
 });
 
-app.listen(port, ()=>{
+app.post('/login', async (req, res) => {
+    try {
+        const data = req.body;
+        const result = await userdata.find({username: data.username});
+        console.log(result);
+        if(result.length == 0){
+            res.send('user not registered');
+        }else{
+            if(result[0].password == data.password){
+                res.send('login successfull');
+            }else{
+                res.send('invalid username/password');
+            };
+        };
+    } catch (e) {
+        console.log(e);
+    };
+})
+
+app.listen(port, () => {
     console.log(`server is running on port no ${port}`);
 });
